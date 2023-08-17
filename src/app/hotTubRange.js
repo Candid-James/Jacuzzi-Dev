7259547ece075090a7a78698da1f9cfa6bff36f2
James Battye <140390470+Candid-James@users.noreply.github.com>
2023-08-17 18:58:15 +0200

    adjusted titles

--- src/app/hotTubRange.js
+++ src/app/hotTubRange.js
@@ -1,7 +1,6 @@
 import { createFullHeightSlider } from 'src/utils/fullHeightSlider.mjs';
 import { createHotTubOptionSlider } from 'src/utils/hotTubOptionSlider.mjs';
 
-// Call the fetchAndAppendElement function when the page has finished loading
 window.addEventListener('DOMContentLoaded', function () {
   createHotTubOptionSlider();
   createFullHeightSlider();
--- src/modules/featuredItemSlider.js
+++ src/modules/featuredItemSlider.js
@@ -1,13 +1,11 @@
-import { getFirstWord } from 'src/utils/getClassName.mjs';
 import { Swiper } from 'swiper';
+import { getFirstWord } from 'src/utils/getClassName.mjs';
 
 export function featureItemSlider() {
-  console.log('featured item slider fired');
   const identifier = 'featured';
   const sliders = document.querySelectorAll(`[${identifier}-element='slider-component']`);
 
   if (window.innerWidth < 767) {
-    console.log(sliders);
     sliders.forEach((e) => {
       console.log('found:' + e);
       const wrapper = e.querySelector(`[${identifier}-element='wrapper']`);
@@ -15,8 +13,6 @@ export function featureItemSlider() {
       const item = e.querySelector(`[${identifier}-element='item']`);
       const listClass = getFirstWord(list);
       const itemClass = getFirstWord(item);
-
-      console.log({ wrapper, list, item, listClass, itemClass });
 
       if (wrapper) {
         const swiper = new Swiper(wrapper, {
