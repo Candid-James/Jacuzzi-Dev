958582e0ab4e06972efd8e302a605cf0d11bfedb
James Battye <140390470+Candid-James@users.noreply.github.com>
2023-08-23 14:00:25 +0200

    Fixed issue on downloadBrochure page

--- src/app/downloadBrochure.js
+++ src/app/downloadBrochure.js
@@ -2,7 +2,7 @@ import { contactZipCodeValidation } from 'src/modules/contactFormat.js';
 
 window.addEventListener('DOMContentLoaded', function () {
   const button = document.getElementById('submitButton');
-  const countryDropdown = document.getElementById('Download-Bochure-Country-Code');
-  const zipInput = document.getElementById('Download-Bochure-Zip-Code');
+  const countryDropdown = document.getElementById('Download-Brochure-Country-Code');
+  const zipInput = document.getElementById('Download-Brochure-Zip-Code');
   contactZipCodeValidation(button, countryDropdown, zipInput);
 });
