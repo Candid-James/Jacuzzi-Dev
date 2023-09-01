f4efcfd15a665e162fe88949249d8edd2eb4b3df
James Battye <140390470+Candid-James@users.noreply.github.com>
2023-09-01 08:16:29 +0200

    Added slider to download brochure

--- src/app/downloadBrochure.js
+++ src/app/downloadBrochure.js
@@ -1,8 +1,10 @@
 import { contactZipCodeValidation } from 'src/modules/contactFormat.mjs';
+import { featureItemSlider } from 'src/modules/featuredItemSlider.mjs';
 
 window.addEventListener('DOMContentLoaded', function () {
   const button = document.getElementById('submitButton');
   const countryDropdown = document.getElementById('Download-Brochure-Country-Code');
   const zipInput = document.getElementById('Download-Brochure-Zip-Code');
   contactZipCodeValidation(button, countryDropdown, zipInput);
+  featureItemSlider();
 });
