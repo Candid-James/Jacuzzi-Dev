e48054dc549041b9db00344233d8654b29cced69
James Battye <140390470+Candid-James@users.noreply.github.com>
2023-09-08 12:10:57 +0200

    Improved mobile nav

--- src/app/navAndFooter.js
+++ src/app/navAndFooter.js
@@ -1,27 +1,35 @@
-// Cache DOM elements
-const wrapper = document.querySelector('.nav_menu-list');
-const nav = document.querySelector('.nav');
-const secondLayer = document.querySelector('[second-layer]');
-const hotTubToggle = document.querySelector('[open-hot-tub-modal]');
-const layerLink = document.querySelector('[open-second-layer]');
-const backButton = document.querySelector('[menu-back-button]');
-const closeButton = document.querySelector('[close-side-menu]');
-const openSide = document.querySelector('[open-side-modal]');
-const hotTubsMobile = document.querySelector('[open-third-layer]');
-const firstLayer = document.querySelectorAll('.nav_menu-layer');
-const firstLayerLinks = firstLayer[0].querySelectorAll('.nav_menu-link');
 function navInit() {
+  // Cache DOM elements
+  const wrapper = document.querySelector('.nav_menu-list');
+  const nav = document.querySelector('.nav');
+  const secondLayer = document.querySelector('[second-layer]');
+  const hotTubToggle = document.querySelector('[open-hot-tub-modal]');
+  const layerLink = document.querySelector('[open-second-layer]');
+  const backButton = document.querySelector('[menu-back-button]');
+  const closeButton = document.querySelector('[close-side-menu]');
+  const openSide = document.querySelector('[open-side-modal]');
+  const hotTubsMobile = document.querySelector('[open-third-layer]');
+  const firstLayer = document.querySelectorAll('.nav_menu-layer');
+  const firstLayerLinks = firstLayer[0].querySelectorAll('.nav_menu-link');
+
   let lastScrollTop = 0;
   let isNavHidden = false;
 
   window.addEventListener('scroll', function () {
     let currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
+    // Check if top of the viewport is within 100px of the top of the page
+    if (currentScrollTop <= 100) {
+      gsap.to('.nav', { duration: 0.5, y: '0%' });
+      isNavHidden = false;
+      return; // Exit out of the function early
+    }
+
     // Determine scroll direction
     if (currentScrollTop > lastScrollTop) {
       // Scrolling down
       if (!isNavHidden) {
-        gsap.to('.nav', { duration: 0.5, y: '-101%' });
+        gsap.to('.nav', { duration: 0.5, y: '-100%' });
         isNavHidden = true;
       }
     } else {
@@ -162,29 +170,31 @@ function navInit() {
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-  document.addEventListener('click', (e) => {
-    const menuWrapper = document.querySelector('.nav_menu-wrapper');
-    const hotTubWraper = document.querySelector('.nav_hot-tubs-wrapper');
-    if (
-      !menuWrapper.contains(e.target) &&
-      !hotTubWraper.contains(e.target) &&
-      !e.target.matches('button, a') &&
-      !(e.target instanceof HTMLDivElement && e.target.innerHTML === 'Hot Tubs')
-    ) {
-      if (nav.classList.contains('side-open')) {
-        nav.classList.remove('side-open');
-      }
-
-      if (nav.classList.contains('hot-tub-open')) {
-        nav.classList.remove('hot-tub-open');
-      }
-    }
-  });
   const observer = new MutationObserver((mutations, observerInstance) => {
     const navElement = document.querySelector('.nav');
 
     if (navElement) {
       navInit();
+      document.addEventListener('click', (e) => {
+        // Cache DOM elements
+        const nav = document.querySelector('.nav');
+        const menuWrapper = document.querySelector('.nav_menu-wrapper');
+        const hotTubWraper = document.querySelector('.nav_hot-tubs-wrapper');
+        if (
+          !menuWrapper.contains(e.target) &&
+          !hotTubWraper.contains(e.target) &&
+          !e.target.matches('button, a') &&
+          !(e.target instanceof HTMLDivElement && e.target.innerHTML === 'Hot Tubs')
+        ) {
+          if (nav.classList.contains('side-open')) {
+            nav.classList.remove('side-open');
+          }
+
+          if (nav.classList.contains('hot-tub-open')) {
+            nav.classList.remove('hot-tub-open');
+          }
+        }
+      });
       observerInstance.disconnect(); // Stops the observer once the .nav element is found and processed.
     }
   });
