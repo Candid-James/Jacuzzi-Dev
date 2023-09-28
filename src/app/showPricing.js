5c5ec6aef530be53783cfa4326e5cb58d2072825
Ezly Elkana Jaulih <40875074+ezly@users.noreply.github.com>
2023-09-28 11:34:34 +0800

    Update showPricing.js
    Test branch

--- src/app/showPricing.js
+++ src/app/showPricing.js
@@ -1,57 +1,59 @@
 // Import required functions from the 'showPricingFunctions.mjs' module.
 import {
-  getCookieValue,
-  showPricingFalseTag,
-  showPricingTrueTag,
+    getCookieValue,
+    showPricingFalseTag,
+    showPricingTrueTag,
 } from 'src/modules/showPricingFunctions.mjs';
 
 window.addEventListener('DOMContentLoaded', () => {
-  // Select all elements with 'j-element' attribute set to 'show-pricing-true'.
-  const pricingTrueTag = document.querySelectorAll('[j-element="show-pricing-true"]');
+    // Select all elements with 'j-element' attribute set to 'show-pricing-true'.
+    const pricingTrueTag = document.querySelectorAll('[j-element="show-pricing-true"]');
 
-  // Select all elements with 'j-element' attribute set to 'show-pricing-false'.
-  const pricingFalseTag = document.querySelectorAll('[j-element="show-pricing-false"]');
+    // Select all elements with 'j-element' attribute set to 'show-pricing-false'.
+    const pricingFalseTag = document.querySelectorAll('[j-element="show-pricing-false"]');
 
-  /**
-   * Function to hide both types of pricing tags.
-   */
-  function hidePricingTag() {
-    // Iterate through all elements of 'pricingTrueTag' and set their display style to 'none' (hidden).
-    pricingTrueTag?.forEach((el) => {
-      el.style.display = 'none';
-    });
+    /**
+     * Function to hide both types of pricing tags.
+     */
+    function hidePricingTag() {
+        // Iterate through all elements of 'pricingTrueTag' and set their display style to 'none' (hidden).
+        pricingTrueTag.forEach((el) => {
+            el.style.display = 'none';
+        })
 
-    // Iterate through all elements of 'pricingFalseTag' and set their display style to 'none' (hidden).
-    pricingFalseTag?.forEach((el) => {
-      el.style.display = 'none';
-    });
-  }
-
-  /**
-   * Function to check the value of the 'show-pricing' cookie.
-   * Depending on the value, it will either show the pricingTrueTag or the pricingFalseTag.
-   */
-  function checkCookie() {
-    if (getCookieValue('show-pricing')) {
-      showPricingTrueTag(pricingTrueTag);
-    } else {
-      showPricingFalseTag(pricingFalseTag);
+        // Iterate through all elements of 'pricingFalseTag' and set their display style to 'none' (hidden).
+        pricingFalseTag.forEach((el) => {
+            el.style.display = 'none';
+        })
     }
-  }
 
-  // Default behavior when the website is loaded:
-  // All elements with 'j-element' attribute set to 'show-pricing' will be hidden by default.
-  // Then, the script will check for the 'show-pricing' cookie.
-  // If the cookie has a value of 'true', the pricingTrueTag will be displayed.
-  // Function to define the default behavior.
-  function defaultBehaviour() {
-    // // Hide all pricing tags by default.
-    hidePricingTag();
+    /**
+     * Function to check the value of the 'show-pricing' cookie.
+     * Depending on the value, it will either show the pricingTrueTag or the pricingFalseTag.
+     */
+    function checkCookie() {
+        if (getCookieValue('show-pricing')) {
+            showPricingTrueTag(pricingTrueTag);
+        } else {
+            showPricingFalseTag(pricingFalseTag);
+        }
+    }
 
-    // Check for the 'show-pricing' cookie and display the appropriate tag.
-    checkCookie();
-  }
+    // Default behavior when the website is loaded:
+    // All elements with 'j-element' attribute set to 'show-pricing' will be hidden by default.
+    // Then, the script will check for the 'show-pricing' cookie.
+    // If the cookie has a value of 'true', the pricingTrueTag will be displayed.
+    // Function to define the default behavior.
+    function defaultBehaviour() {
+        // // Hide all pricing tags by default.
+        hidePricingTag();
 
-  // Execute the default behavior.
-  defaultBehaviour();
+        // Check for the 'show-pricing' cookie and display the appropriate tag.
+        checkCookie();
+    }
+
+    // Execute the default behavior.
+    defaultBehaviour();
+
+    console.log('test github')
 });
